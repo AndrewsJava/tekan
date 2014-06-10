e1d294b9d908f7cc3eaf20dce5a0f8d4ee0902a2
Andrew Parelius <parelius@uw.edu>
2014-06-10 08:57:38 -0500

    added title; search; seach submit with enter

--- src/harlequinmettle/finance/technicalanalysis/TechnicalDatabaseViewer.java
+++ src/harlequinmettle/finance/technicalanalysis/TechnicalDatabaseViewer.java
@@ -1,8 +1,11 @@
 package harlequinmettle.finance.technicalanalysis;
 
 import harlequinmettle.utils.guitools.JScrollPanelledPane;
+import harlequinmettle.utils.guitools.JSearchPanel;
 import harlequinmettle.utils.guitools.PreferredJScrollPane;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
@@ -12,49 +15,64 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
-public class TechnicalDatabaseViewer extends JTabbedPane{
+public class TechnicalDatabaseViewer extends JTabbedPane {
 
 	TechnicalDatabase db = new TechnicalDatabase();
-	
-	public static void main(String[] arg){
+
+	public static void main(String[] arg) {
 		TechnicalDatabaseViewer tdbviewer = new TechnicalDatabaseViewer();
-	
+
 	}
-	
-	public TechnicalDatabaseViewer(){
+
+	public TechnicalDatabaseViewer() {
 		JFrame container = new JFrame();
 		container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		container.setSize(800,500);
+		container.setSize(800, 500);
 		container.add(this);
 		container.setVisible(true);
 		JScrollPanelledPane controls = new JScrollPanelledPane();
-		this.add("controls",controls);
-	showChartInNewWindow();
+		this.add("controls", controls);
+		  JSearchPanel searchPanel = new JSearchPanel();
+		searchPanel.addSearchAction(doSearchActionListener(searchPanel));
+		controls.addComp(searchPanel);
 	}
 
-	private void showChartInNewWindow() {
-		final JFrame container = new JFrame();		
-		 
+	private ActionListener doSearchActionListener(final JSearchPanel searchPanel) {
+		return new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			//getParent()
+
+				showChartInNewWindow(searchPanel.getSearchText().trim().toUpperCase());
+			}
+			
+		};
+	}
+
+	private void showChartInNewWindow(String ticker) {
+		final JFrame container = new JFrame(ticker+"   "+TechnicalDatabase.NUM_DAYS+ "  days");
+
 		container.setSize(900, 550);
 		container.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		container.setVisible(true);
 		JScrollPanelledPane chart = new JScrollPanelledPane();
-		TickerTechView tv = new TickerTechView();
+		TickerTechView tv = new TickerTechView(ticker);
 		PreferredJScrollPane tickerTechScroll = new PreferredJScrollPane(tv);
-		 chart.addComp(tickerTechScroll);
-		 
-		container.add(chart );
+		chart.addComp(tickerTechScroll);
+
+		container.add(chart);
 		container.setExtendedState(container.getExtendedState()
 				| JFrame.MAXIMIZED_BOTH);
-		final ComponentListener refForRemoval =  doWindowRescaleListener(tv);
+		final ComponentListener refForRemoval = doWindowRescaleListener(tv);
 		container.addComponentListener(refForRemoval);
-		container.addWindowListener(new WindowAdapter(){
-					  @Override 
-			            public void windowClosing(WindowEvent e) { 
-						  container.removeComponentListener(refForRemoval);
-					  }
-				}); 
+		container.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				container.removeComponentListener(refForRemoval);
+			}
+		});
 	}
 
 	private ComponentListener doWindowRescaleListener(final TickerTechView tv) {
@@ -67,5 +85,5 @@ public class TechnicalDatabaseViewer extends JTabbedPane{
 
 			}
 		};
-	} 
+	}
 }

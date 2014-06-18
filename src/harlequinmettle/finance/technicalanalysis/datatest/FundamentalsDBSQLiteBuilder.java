7beef5513a54b7d4cd70a85fbb8fba70d30ddf92
Andrew Parelius <parelius@uw.edu>
2014-06-18 15:32:56 -0500

    storms comming

--- src/harlequinmettle/finance/technicalanalysis/datatest/FundamentalsDBSQLiteBuilder.java
+++ src/harlequinmettle/finance/technicalanalysis/datatest/FundamentalsDBSQLiteBuilder.java
@@ -6,6 +6,7 @@ import harlequinmettle.utils.filetools.sqlite.SQLiteTools;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Arrays;
 
 public class FundamentalsDBSQLiteBuilder {
 
@@ -16,12 +17,16 @@ public class FundamentalsDBSQLiteBuilder {
 
 	private void buildDB() {
 		Connection cn = SQLiteTools
-				.establishSQLiteConnection("FUNDAMENTSLSDATABASE");
+				.establishSQLiteConnection("FUNDAMENTALSDATABASE");
 		if (cn != null) {
 			String tableName = "fundamentals";
+			String[] columnEntries = doColumnentries();
+			String[] types = new String[columnEntries.length];
+			Arrays.fill(types, "real");
 			// Statement used for query
 			Statement stat = SQLiteTools.reinitializeTable(cn, tableName,
-					CurrentFundamentalsDatabase.forDisplaying);
+					columnEntries,types);
+			
 			// TODO:FIXME: SEE SQLiteTester for SQLite statement exectution
 			//
 			// PreparedStatement prep = cn
@@ -33,6 +38,14 @@ public class FundamentalsDBSQLiteBuilder {
 			//
 
 		}
+	}
+
+	private String[] doColumnentries() {
+		String entries = "ticker dateOfCollection ";
+	for(String s: CurrentFundamentalsDatabase.forDisplaying){
+		entries+=s.replaceAll("[^A-Za-z0-9]", "_")+" ";
+	}
+	return entries.trim().split(" ");
 	}
 
 }

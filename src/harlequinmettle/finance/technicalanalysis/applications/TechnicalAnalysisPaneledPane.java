ed6f19a0b3a25e6c32f8d3756607c500b44a9f57
Andrew Parelius <parelius@uw.edu>
2014-07-09 12:21:47 -0500

    added filtering to standard deviation technical data

--- src/harlequinmettle/finance/technicalanalysis/applications/TechnicalAnalysisPaneledPane.java
+++ src/harlequinmettle/finance/technicalanalysis/applications/TechnicalAnalysisPaneledPane.java
@@ -2,7 +2,9 @@ package harlequinmettle.finance.technicalanalysis.applications;
 
 import harlequinmettle.finance.technicalanalysis.model.db.CurrentFundamentalsSQLiteDatabase;
 import harlequinmettle.finance.technicalanalysis.model.db.TechnicalDatabaseSQLite;
+import harlequinmettle.finance.technicalanalysis.view.TickerButtonsScrollingPanel;
 import harlequinmettle.utils.guitools.FilterPanel;
+import harlequinmettle.utils.guitools.JLabelFactory;
 import harlequinmettle.utils.guitools.JScrollPanelledPane;
 import harlequinmettle.utils.numbertools.math.statistics.BasicCalculations;
 import harlequinmettle.utils.numbertools.math.statistics.StatInfo;
@@ -23,19 +25,31 @@ public class TechnicalAnalysisPaneledPane extends JScrollPanelledPane {
 	public TechnicalAnalysisPaneledPane() {
 		init();
 	}
-
+String[] technicalFilterOptions = {"stdev daily price change"};
 	private void init() {
+		addComp(JLabelFactory.doBluishJLabel("Technical Filters"));
+		final FilterPanel technical_filter_one = new FilterPanel(technicalFilterOptions);
+		final FilterPanel[] technicalIndicatorfilters = { technical_filter_one };
+		for (FilterPanel fp : technicalIndicatorfilters) {
+			fp.setFontSize(40);
+			addComp(fp);
+		}
+		JButton t = new JButton("filter technicals");
+		t.addActionListener(doTechnicalFilterListener(TechnicalDatabaseViewer.TDB, technicalIndicatorfilters));
+		addComp(t);
+		
 		final FilterPanel filter_one = new FilterPanel(TechnicalDatabaseViewer.FDB.subsetLabels);
 		final FilterPanel filter_two = new FilterPanel(TechnicalDatabaseViewer.FDB.subsetLabels);
 		final FilterPanel filter_three = new FilterPanel(TechnicalDatabaseViewer.FDB.subsetLabels);
 		final FilterPanel filter_four = new FilterPanel(TechnicalDatabaseViewer.FDB.subsetLabels);
 		final FilterPanel filter_five = new FilterPanel(TechnicalDatabaseViewer.FDB.subsetLabels);
 		final FilterPanel[] fundamentalIndicatorfilters = { filter_one, filter_two, filter_three, filter_four, filter_five };
+		addComp(JLabelFactory.doBluishJLabel("Fundamentals Filters"));
 		for (FilterPanel fp : fundamentalIndicatorfilters) {
 			fp.setFontSize(40);
 			addComp(fp);
 		}
-		JButton b = new JButton("filter and analyze");
+		JButton b = new JButton("filter fundamentals and analyze");
 		b.addActionListener(doFilterAndAnalyzeListener(TechnicalDatabaseViewer.FDB, fundamentalIndicatorfilters));
 		addComp(b);
 
@@ -43,6 +57,28 @@ public class TechnicalAnalysisPaneledPane extends JScrollPanelledPane {
 		// c.addActionListener(generateSQLiteContructorActionListener());
 		// addComp(c);
 
+	}
+
+	private ActionListener doTechnicalFilterListener(final TechnicalDatabaseSQLite tDB, final FilterPanel[] technicalIndicatorfilters) {
+
+		return new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				TreeMap<String, Float> stdev =   analyzeResults( tDB.SQLITE_PER_TICKER_PER_DAY_TECHNICAL_DATA.keySet());
+				  ArrayList<String> filtered = 	new ArrayList<String>();
+				  for(Entry<String,Float> ent: stdev.entrySet()){
+					  //TODO coordinate properly mulitple technical filters
+					  //for now just the one metric daily price change
+					  float lowStDev = technicalIndicatorfilters[0].getLow();
+					float highStDev =   technicalIndicatorfilters[0].getHigh();
+					float stdv = ent.getValue();
+					if(stdv<=highStDev && stdv >= lowStDev){
+						filtered.add(ent.getKey());
+					}
+				  }
+				  new TickerButtonsScrollingPanel(filtered,technicalIndicatorfilters[0].getFilterName());
+			} 
+		};
 	}
 
 	private ActionListener doFilterAndAnalyzeListener(final CurrentFundamentalsSQLiteDatabase fdb, final FilterPanel[] filters) {
@@ -57,15 +93,15 @@ public class TechnicalAnalysisPaneledPane extends JScrollPanelledPane {
 						title += " " + fp.getFilterName();
 				}
 				System.out.println(filterResults.size() + "    (:U)-H RESULTS");
-				StatInfo results = analyzeResults(filterResults.keySet());
+				StatInfo results = new StatInfo(new ArrayList<Float>(analyzeResults(filterResults.keySet()).values()));
 			}
 
 		};
 	}
 
-	protected StatInfo analyzeResults(Collection<String> values) {
+	protected TreeMap<String,Float> analyzeResults(Collection<String> values) {
 		int nullcount = 0;
-		ArrayList<Float> standardDeviations = new ArrayList<Float>();
+		TreeMap<String,Float> standardDeviations = new TreeMap<String,Float>();
 		for (String ticker : values) {
 			float[][] techData = TechnicalDatabaseViewer.TDB.SQLITE_PER_TICKER_PER_DAY_TECHNICAL_DATA.get(ticker);
 			if (techData == null) {
@@ -85,9 +121,9 @@ public class TechnicalAnalysisPaneledPane extends JScrollPanelledPane {
 				target[i] = totalChanges.get(i);
 			}
 			float standardDev = (float) new StandardDeviation().evaluate(target);
-			standardDeviations.add(standardDev);
+			standardDeviations.put(ticker,standardDev);
 		}
-		return new StatInfo(standardDeviations);
+		return  (standardDeviations );
 	}
 
 }
